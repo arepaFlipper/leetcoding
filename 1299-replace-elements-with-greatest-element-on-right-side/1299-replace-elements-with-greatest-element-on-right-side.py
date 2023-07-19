9fe89b0d5173c740b6171baf2fb39809346cca29
Cristian F. Tovar <cristian00tovar@gmail.com>
2023-07-19 17:06:39 -0500

    💙 1929 concatenation-of-array debugged:
    - it starts iterating by the end and keeps its indexes
    - it always saves the previous greatest number, is for that reason that
    the initial value saved is -1

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.py
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.py
@@ -1,13 +1,30 @@
+# !/usr/bin/env python
+from typing import List
 class Solution:
     def replaceElements(self, arr: List[int]) -> List[int]:
-        ans = [0 for el in arr]
-        for i, idx in enumerate(arr):
-            greatest = -1
-            cur_sub = arr[i:]
-            for j in range(len(cur_sub)):
-                cur= cur_sub[j]
-                if cur > greatest:
-                    greatest = cur
-                ans[j] = greatest
-        
-        return ans
+        greatest = -1
+        for j in range(len(arr)-1,-1,-1):
+            print("j: "+str(j)) ## DELETEME:
+            cur = max(arr[j], greatest)
+            print("cur: "+str(cur)) ## DELETEME:
+            print("bef arr: "+str(arr)) ## DELETEME:
+            arr[j] = greatest
+            print("af arr: "+str(arr)) ## DELETEME:
+            greatest = cur
+        return arr
+
+if __name__ == "__main__":
+    solution = Solution()
+
+# Test Case 1
+    arr1 = [17, 18, 5, 4, 6, 1]
+    output1 = solution.replaceElements(arr1)
+    print("Test Case 1:", output1)
+# Expected output: [18, 6, 6, 6, 1, -1]
+
+# Test Case 2
+    arr2 = [400]
+    output2 = solution.replaceElements(arr2)
+    print("Test Case 2:", output2)
+# Expected output: [-1]
+
