c1c10050f81f1cf1d98f3fd2800503873e03958c
Cristian F. Tovar <cristian00tovar@gmail.com>
2023-07-20 06:07:56 -0500

    👞 392 is-subsequence (88.94% time)

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -1,20 +1,28 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        idx= 0
-        for c in s:
-            if c in t:
-                idx = t.index(c)
-            else:
-                return False
-            idx += 1
-            t = t[idx:]
-        return True
+        i , j = 0,0
+        while (i < len(s)) and (j<len(t)):
+            if s[i] == t[j]:
+                i += 1
+            j += 1
+
+        if i == len(s):
+            return True
+        else:
+            return False
 
 if __name__ == '__main__':
     solution = Solution()
     
     # Test Cases
-    print("Test Case 1:", solution.isSubsequence("twn", "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxtxxxxxxxxxxxxxxxxxxxxwxxxxxxxxxxxxxxxxxxxxxxxxxn"))
-    print("Test Case 2:", solution.isSubsequence("aaaaaa", "bbaaaa"))
-    print("Test Case 3:", solution.isSubsequence("abc", "ahbgdc"))
-    print("Test Case 4:", solution.isSubsequence("axc", "ahbgdc"))
+    test_1 = solution.isSubsequence("twn", "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxtxxxxxxxxxxxxxxxxxxxxwxxxxxxxxxxxxxxxxxxxxxxxxxn")
+    print("Test Case 1:", test_1, "✔️ " if test_1 else "❌")
+
+    test_2 = solution.isSubsequence("aaaaaa", "bbaaaa")
+    print("Test Case 2:", test_2, "✔️ " if not test_2 else "❌")
+
+    test_3 = solution.isSubsequence("abc", "ahbgdc")
+    print("Test Case 3:", test_3, "✔️ " if test_3 else "❌")
+
+    test_4 = solution.isSubsequence("axc", "ahbgdc")
+    print("Test Case 4:", test_4, "✔️ " if not test_4 else "❌")
