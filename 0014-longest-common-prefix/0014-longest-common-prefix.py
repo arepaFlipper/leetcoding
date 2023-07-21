956ec1521820460c7dd74c597f2e2617662b40d7
Cristian F. Tovar <cristian00tovar@gmail.com>
2023-07-21 05:56:17 -0500

    ⚔️  14 longest-common-prefix self-made:
    There are shorter and faster solutions but I am proud of my own one,
    because I applied Hashmap.

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,26 +1,27 @@
 class Solution:
-    def find_shortest(self, strs: List[str]):
-        shortest = strs[0]
-        for word in strs:
-            if len(shortest) > len(word):
-                shortest = word
-        return shortest
+    # def find_shortest(self, words: List[str]):
+    #     shortest = words[0]
+    #     for word in words:
+    #         if len(shortest) > len(word):
+    #             shortest = word
+    #     return shortest
 
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        prefix=""
-        idx = 0
-        shortest_word = self.find_shortest(strs)
-        print("shortest: ",shortest_word) ## DELETEME:
-        while (idx < len(shortest_word)-1):
-            char = shortest_word[idx]
-            for word in strs:
-                print("word: ",word) ## DELETEME:
-                if char != word[idx]:
-                    idx = len(shortest_word)
-                    break
-            prefix += char
-            if (idx < len(shortest_word)):
+        prefix: str = ""
+        count_chars: dict = defaultdict(int)
+        for word in strs:
+            for idx in range(len(word)):
+                c:Char = word[idx]
+                key: str = c + str(idx)
+                count_chars[key] += 1
+            
+        shortest_word: str = strs[0]
+        print("count_chars:",count_chars) ## DELETEME:
+        for index in range(len(shortest_word)):
+            char: Char = shortest_word[index]
+            k: str = char + str(index)
+            if count_chars[k] == len(strs):
+                prefix += char
+            else:
                 break
-            idx += 1
-            print("prefix: ",prefix) ## DELETEME:
         return prefix
