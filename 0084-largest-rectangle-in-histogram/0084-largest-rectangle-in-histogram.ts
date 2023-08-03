21de0dd5507f4a00233c8662cd0e69b2399b6f18
Cristian F. Tovar <cristian00tovar@gmail.com>
2023-08-03 07:46:18 -0500

    🍋 84 largest rectangle in histogram:
    Use while and regular for loops to optimize time complexity.

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.ts
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.ts
@@ -4,28 +4,28 @@ type pair = {
 };
 
 function largestRectangleArea(heights: number[]): number {
-    let max_area = 0;
-    let stack = [];
-    // for (let i = 0; i < heights.length; i++) {
-  
-    heights.forEach((curr_h, i) => {
-        let start = i;
-        while (stack.length > 0 && stack[stack.length - 1].height > curr_h) {
-            let { start_x, height } = stack.pop();
-            const width = i - start_x;
-            const area = height * width;
-            max_area = Math.max(max_area, area);
-            start = start_x;
-        }
-        stack.push({start_x: start, height:curr_h});
-    });
+  let max_area = 0;
+  let stack: pair[] = [];
 
-    stack.forEach(({start_x, height}, jdx) => {
-      const current_area = height * (heights.length - start_x );
-      max_area = Math.max(max_area, current_area);
-    });
+  for (let i = 0; i < heights.length; i++) {
+    let start = i;
+    while (stack.length > 0 && stack[stack.length - 1].height > heights[i]) {
+      const { start_x, height } = stack.pop()!;
+      const width = i - start_x;
+      const area = height * width;
+      max_area = Math.max(max_area, area);
+      start = start_x;
+    }
+    stack.push({ start_x: start, height: heights[i] });
+  }
 
-    return max_area;
-};
+  while (stack.length > 0) {
+    const { start_x, height } = stack.pop()!;
+    const width = heights.length - start_x;
+    const area = height * width;
+    max_area = Math.max(max_area, area);
+  }
 
+  return max_area;
+}
 
