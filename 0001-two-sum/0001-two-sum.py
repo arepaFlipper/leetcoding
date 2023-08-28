0c93a63be62bd52df7fca05ba62945d6c53ad3df
Cristian F. Tovar <cristian00tovar@gmail.com>
2023-08-28 13:10:45 -0500

    💎 167 two sum II input array is sorted

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,7 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            for j in range(len(nums)):
-                if(nums[i]+nums[j]) == target:
-                    return [i,j]
-                    
+        hm: Dict = {}
+        for (idx, num) in enumerate(nums):
+            diff: int = target - num
+            if (diff in hm):
+                return [idx, hm[diff]]
+            hm[num] = idx
+
