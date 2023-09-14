067596fffb7546869e2d6b043ee96f4333a55101
Cristian F. Tovar <cristian00tovar@gmail.com>
2023-09-14 11:59:20 -0500

    😕 567 permutation-in-string

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -8,11 +8,15 @@ class Solution:
         for i in range(len(s1)):
             s1_count[ord(s1[i])- ord ('a')] += 1
             s2_count[ord(s2[i])- ord ('a')] += 1
+            print("s1_count:",s1_count) ## DELETEME:
+            print("s2_count:",s2_count) ## DELETEME:
         
         matches = 0
 
         for i in range(26):
-            matches += (1 if s1_count[i] == s2_count[i] else 0)
+            if s1_count[i] == s2_count[i]:
+                matches += 1
+        print("matches:",matches) ## DELETEME:
 
         l = 0
         for r in range(len(s1), len(s2)):
@@ -29,9 +33,24 @@ class Solution:
             
             index = ord(s2[l]) - ord('a')
             s2_count[index] -=1
+
             if s1_count[index] == s2_count[index]:
                 matches += 1
             elif s1_count[index] -1 == s2_count[index]:
                 matches -= 1
             l += 1
+
         return matches == 26
+# Test the function
+solution = Solution()
+# s1 = "ab"
+# s2 = "eidbaooo"
+# result = solution.checkInclusion(s1, s2)
+s1 = "abc"
+s2 = "baxyzabc"
+result = solution.checkInclusion(s1, s2)
+
+if result:
+    print(f'"{s1}" is a permutation of "{s2}"')
+else:
+    print(f'"{s1}" is not a permutation of "{s2}"')
