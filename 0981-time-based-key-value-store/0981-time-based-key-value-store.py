d413d17cdd961830c7e592b699141a19d93a34c9
Cristian F. Tovar <cristian00tovar@gmail.com>
2023-10-18 15:03:00 -0500

    💭 981 time based key value store TESTS

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.py
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.py
@@ -12,18 +12,48 @@ class TimeMap:
         values = self.store.get(key, [])
 
         left = 0
-        right = len(values) -1
+        right = len(values) - 1
         while left <= right:
-            med = (left+right) // 2
+            med = (left + right) // 2
             if values[med][1] <= timestamp:
-                res =values[med][0]
+                res = values[med][0]
                 left = med + 1
             else:
                 right = med - 1
 
         return res
-        
-# Your TimeMap object will be instantiated and called as such:
-# obj = TimeMap()
-# obj.set(key,value,timestamp)
-# param_2 = obj.get(key,timestamp)
+
+def test_time_based_key_value_store(operations, values):
+    timeMap = TimeMap()
+    output = []
+
+    for i in range(len(operations)):
+        operation = operations[i]
+        value = values[i]
+
+        if operation == "set":
+            key, val, timestamp = value
+            timeMap.set(key, val, timestamp)
+            output.append(None)
+        elif operation == "get":
+            key, timestamp = value
+            result = timeMap.get(key, timestamp)
+            output.append(result)
+
+    expected = [None, "bar", "bar",None, "bar2", "bar2"]
+    print("Input operations:", operations)
+    print("Input values:", values)
+    print("Output:", output)
+    print("Expected:", expected)
+
+    if output == expected:
+        print("✅ Test passed")
+    else:
+        print("❌ Test failed")
+
+# Test case
+operations = ["TimeMap", "set", "get", "get", "set", "get", "get"]
+values = [[], ["foo", "bar", 1], ["foo", 1], ["foo", 3], ["foo", "bar2", 4], ["foo", 4], ["foo", 5]]
+
+test_time_based_key_value_store(operations, values)
+
