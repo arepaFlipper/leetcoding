5ff9e2078a4388325e4baf6c2cb0ae4952b99b93
Cristian F. Tovar <cristian00tovar@gmail.com>
2023-11-02 11:53:07 -0500

    🐹 Trees>104 maximum depth of binary tree (BFS)

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -1,3 +1,5 @@
+from collections import deque 
+
 # Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
@@ -9,8 +11,20 @@ class Solution:
     def maxDepth(self, root):
         if not root:
             return 0
+        
+        level = 0
+        queue = deque([root])
 
-        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+        while queue:
+            for i in range(len(queue)):
+                node = queue.popleft()
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            level += 1
+
+        return level
 
 solution = Solution()
 
