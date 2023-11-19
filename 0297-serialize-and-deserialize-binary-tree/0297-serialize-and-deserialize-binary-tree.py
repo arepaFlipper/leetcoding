7d17aba76ba555b05830f4cf45aa4aefb6229533
Cristian F. Tovar <cristian00tovar@gmail.com>
2023-11-18 19:00:27 -0500

    🥇 Trees>297 serialize and deserialize binary tree

--- 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.py
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.py
@@ -1,25 +1,3 @@
-I want you to write the tests to my code in the same manner you've been doing early in this chat, here is my problem:
-
- 297. Serialize and Deserialize Binary Tree 
- Serialization is the process of converting a data structure or object into a sequence of bits so that it can be stored in a file or memory buffer, or transmitted across a network connection link to be reconstructed later in the same or another computer environment. 
- Design an algorithm to serialize and deserialize a binary tree. There is no restriction on how your serialization/deserialization algorithm should work. You just need to ensure that a binary tree can be serialized to a string and this string can be deserialized to the original tree structure. 
- Clarification: The input/output format is the same as how LeetCode serializes a binary tree. You do not necessarily need to follow this format, so please be creative and come up with different approaches yourself. 
-   
- Example 1: 
- https://assets.leetcode.com/uploads/2020/09/15/serdeser.jpg 
- Input: root = [1,2,3,null,null,4,5] 
- Output: [1,2,3,null,null,4,5] 
- Example 2: 
- Input: root = [] 
- Output: [] 
-   
- Constraints: 
- 	The number of nodes in the tree is in the range [0, 104]. 
- 	-1000 <= Node.val <= 1000 
-
-
-The following is my solution to test:
-```
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, x):
@@ -75,4 +53,3 @@ class Codec:
 # ser = Codec()
 # deser = Codec()
 # ans = deser.deserialize(ser.serialize(root))
-```
