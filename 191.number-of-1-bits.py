957d23ad5e3385318e48098247a4fadae1812d1c
Cristian F. Tovar <cristian00tovar@gmail.com>
2024-02-14 19:36:31 -0500

    🌶️ I rather this solution over the previous 191

--- 191.number-of-1-bits.py
+++ 191.number-of-1-bits.py
@@ -3,8 +3,8 @@ class Solution:
     def hammingWeight(self, n: int) -> int:
         res: int = 0
         while n:
-            n &= n - 1
-            res += 1
+            res += n % 2
+            n = n >> 1
         return res
 # @leet end
 # Test Case 1
