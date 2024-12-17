19be5e621c7b1d0cdaf6e7af2b6e36dd3606b74a
Cristian F. Tovar <Christopher@Cristians-MacBook-Pro.local>
2024-12-17 15:15:52 -0500

    📜 the actual code working

--- 704.binary-search.py
+++ 704.binary-search.py
@@ -4,27 +4,36 @@ from typing import List
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         (left, right) = (0, len(nums))
-        med = (left + right) // 2
 
         while left <= right:
-            current = nums[med]
+            mid = (left + right) //2
+            current = nums[mid]
+
             if current == target:
-                return med
-            elif current > target:
-                right = med - 1
-            elif current < target:
-                left = med + 1
-            med = (left + right) // 2
-            print("""🦌   \x1b[1;33;40m704.binary-search.py:18  med:""") ## DELETEME:
-            print(med) ## DELETEME:
-            print('\x1b[0m') ## DELETEME:
+                return mid
+
+            if target < current:
+                right = mid - 1
+
+            if current < target:
+                left = mid +1
 
         return -1
+        
                 
         
 # @leet end
 
 # Test functions
+def test_binary_search_case_0():
+    solution = Solution()
+    nums = [1,3,3,4,5,6,7,8]
+    target = 8
+    expected = 7
+    result = solution.search(nums, target)
+    assert result == expected, f"Expected {expected}, but got {result}"
+    print("Case 1 succeed 👍")
+
 def test_binary_search_case_1():
     solution = Solution()
     nums = [-1, 0, 3, 5, 9, 12]
@@ -72,11 +81,10 @@ def test_binary_search_case_5():
 
 def main():
     # Simulate running tests
+    test_binary_search_case_0()
     test_binary_search_case_1()
     test_binary_search_case_2()
     test_binary_search_case_3()
-    test_binary_search_case_4()
-    test_binary_search_case_5()
 
 if __name__ == "__main__":
     main()
